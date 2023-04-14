8a20162aa214acef9ff25ac0e5c7f5857c4cb120
effi <effiphil@Effis-MBP.lan>
2023-04-14 23:29:32 +0300

    added project files

---
+++ terraform/argocd/argocd.tf
@@ -0,0 +1,23 @@
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "argocd" {
+  name             = var.namespace
+  namespace        = var.namespace
+  repository       = var.argocd_repo
+  chart            = var.argocd_chart
+  version          = var.argocd_version
+  create_namespace = true
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  values = var.argocd_values
+}
